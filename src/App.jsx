c45491036f16d0c57ee33bd677af36c6bb60ca90
Navin Ayer <ayernavin11@gmail.com>
2026-01-12 15:36:47 +0545

    Folder Structure Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./App.css"
+import Home from "./pages/Home/Home.jsx"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 export const App = () => {
   return (
   <>
-
+  <Router>
+  <Routes>
+    <Route path = "/" element = {<Home/>}> </Route>
+    <Route path = "/login" element = {<Login/>}> </Route>
+    <Route path = "/signup" element = {<SignUp/>}> </Route>
+  </Routes>
+  </Router>
   </>
   )
 }
+
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div>
+        <div>
+            <h2>MiniMart</h2>
+        </div>
+        <div>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Cart</a></li>
+                <li><a href="">Login</a></li>
+                <li><a href="">Signup</a></li>
+            </ul>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
